70a76cfbbfcc71feec55f4c4b95b1e3078903a43
Shubham Pote <shubhampote428@gmail.com>
2026-06-01 19:30:57 +0530

    restore milestone state

--- Server/src/db.ts
+++ Server/src/db.ts
@@ -1,9 +1,10 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
 export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL!,
-    },
-  },
+  adapter,
 });
